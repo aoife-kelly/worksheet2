cfb8cc95dcaef0409a8537c524d4ce6a049aeb5f
aoife <96482169+aoleee@users.noreply.github.com>
2026-01-30 20:59:08 +0000

    this is confusing

--- worksheet2/src/app/app.ts
+++ worksheet2/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('worksheet2');
+  protected readonly title = signal("Week ToDo List");
+
+protected tasks=signal([
+  'create test apps',
+  'practice github',
+  'study typescript basics'
+])
 }
